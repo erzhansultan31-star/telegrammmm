07999892a7130835cc85be8276d00565d948363a
erzhansultan31-star <erzhansultan31@gmail.com>
2026-09-05 02:07:48 +0500

    Delete states.py

--- states.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from aiogram.fsm.state import State, StatesGroup
-
-
-class EditStates(StatesGroup):
-    """Пайдаланушы диалогының күйлері."""
-    waiting_cover_photo = State()    # мұқаба суретін күтуде
-    waiting_author_name = State()    # автор атын күтуде
-    waiting_search_query = State()   # іздеу сөзін күтуде
